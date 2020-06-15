a48d44019fcd8f46abd4eeddcde016b5c6af57b9
AlinaCherkasova <alina.ch1202@gmail.com>
2020-06-15 21:02:09 +0300

    add media queries for section awards and features work

--- src/js/common.js
+++ src/js/common.js
@@ -1,17 +1,29 @@
-$(document).ready(function() {
-	$('.featured-work__image-wrap').magnificPopup({
+$(document).ready(function () {
+  $('.featured-work__image-wrap').magnificPopup({
     type: 'image',
     gallery: {
-      enabled: true
+      enabled: true,
     },
     removalDelay: 300,
-    mainClass: 'mfp-fade'
+    mainClass: 'mfp-fade',
   });
 
   $('.grid').isotope({
     itemSelector: '.grid__item',
     masonry: {
-      columnWidth: 15
-    }
+      columnWidth: 15,
+    },
+  });
+
+  $('.toggle-menu').click(function () {
+    $('.menu__list').toggleClass('menu__toggle');
+
+    $.each($('.menu__items'), function (index, link) {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+      }
+    });
   });
 });
